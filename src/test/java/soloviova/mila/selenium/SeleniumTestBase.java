3dc17dcf6789080ce994ebd690dc1ae08d50e432
lsolovio <liudmyla.soloviova@tideworks.com>
2023-02-01 22:36:05 +0200

    added more Selenium tests

--- src/test/java/soloviova/mila/selenium/SeleniumTestBase.java
+++ src/test/java/soloviova/mila/selenium/SeleniumTestBase.java
@@ -1,10 +1,16 @@
 package soloviova.mila.selenium;
 
 import org.junit.After;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class SeleniumTestBase {
     WebDriver webDriver;
+    JavascriptExecutor jsExecutor;
+    WebDriverWait wait;
+    Actions actions;
 
     @After
     public void quitDriver() {
